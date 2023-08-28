0eb89392d0c075036b621b8855a8694190f7bac6
AndersSpear <60556629+AndersSpear@users.noreply.github.com>
2023-08-28 12:17:04 -0400

    Delete p5 directory

--- p5/flask_app/movies/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, url_for, redirect, request, flash
-from flask_login import current_user
-
-from .. import movie_client
-from ..forms import MovieReviewForm, SearchForm
-from ..models import User, Review
-from ..utils import current_time
