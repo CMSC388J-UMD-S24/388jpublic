7c8cead4aa29632614a9736a39b7995ce8ebf7c0
Nikita Krupin <krupin.nikita0110@gmail.com>
2024-04-19 15:44:52 -0400

    week13 code

--- spring2024/week8/app.py
+++ spring2024/week8/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 import random
+import time
 app = Flask(__name__)
 
 @app.route('/')
 def index_route():
     return render_template('index.html')
 
+@app.route('/week13')
+def week_route():
+    return render_template('week13.html')
+
 # api route for fetch example
 @app.route("/rand")
 def hello():
+    time.sleep(2);
     return str(random.randint(0, 100))
